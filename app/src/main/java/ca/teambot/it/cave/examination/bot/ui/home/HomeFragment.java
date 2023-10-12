e45a23a41091d0b36fc5ffd21b47caad0e8a5f4e
Hassan Noorani <hanoorani1@gmail.com>
2023-10-12 16:26:20 -0400

    Added image to home, with animation.

--- app/src/main/java/ca/teambot/it/cave/examination/bot/ui/home/HomeFragment.java
+++ app/src/main/java/ca/teambot/it/cave/examination/bot/ui/home/HomeFragment.java
@@ -11,7 +11,10 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,9 +30,38 @@ public class HomeFragment extends Fragment
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return view;
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        ImageView fadingImage = rootView.findViewById(R.id.fade_cavebot);
+
+        // Create an AlphaAnimation to fade the image in
+        Animation fadeInAnimation = new AlphaAnimation(0.0f, 1.0f);
+        fadeInAnimation.setDuration(1000); // Adjust the duration as needed (in milliseconds)
+
+        // Set an AnimationListener to control visibility when the animation ends
+        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Make the image visible when the animation ends
+                fadingImage.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        // Start the animation
+        fadingImage.startAnimation(fadeInAnimation);
+
+        return rootView;
     }
+
 
 
 
