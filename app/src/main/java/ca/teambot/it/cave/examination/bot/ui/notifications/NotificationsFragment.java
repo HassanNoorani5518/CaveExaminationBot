f1d0fce1a30b9ccf8864f0f0d4da627b688f1392
Hassan Noorani <hanoorani1@gmail.com>
2023-11-13 02:35:49 -0500

    Added Intent to access storage and pick an image.

--- app/src/main/java/ca/teambot/it/cave/examination/bot/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/ca/teambot/it/cave/examination/bot/ui/notifications/NotificationsFragment.java
@@ -139,6 +139,10 @@ public class NotificationsFragment extends Fragment {
         }
     }
 
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
 
 
 
