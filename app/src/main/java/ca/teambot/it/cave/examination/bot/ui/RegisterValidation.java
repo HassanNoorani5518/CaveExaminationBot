9f673acb416c2f72c0a5c18c6f1678ae970ffce7
AdrianPortaln01489363 <122579247+AdrianPortal9363@users.noreply.github.com>
2023-12-03 21:36:47 -0500

    Created RegisterValidation.java class for junit cases.

---
+++ app/src/main/java/ca/teambot/it/cave/examination/bot/ui/RegisterValidation.java
@@ -0,0 +1,42 @@
+package ca.teambot.it.cave.examination.bot.ui;
+
+import android.content.Context;
+
+import ca.teambot.it.cave.examination.bot.R;
+
+public class RegisterValidation
+{
+    private final Context context;
+
+    public RegisterValidation(Context context)
+    {
+        this.context = context;
+    }
+
+    public boolean isValidName(String name)
+    {
+        return !name.isEmpty();
+    }
+    public boolean isValidEmail(String email)
+    {
+        return !email.isEmpty() && email.matches(context.getString(R.string.a_za_z0_9_a_za_z0_9_a_za_z_2));
+    }
+    public boolean isValidPassword(String password)
+    {
+        return !password.isEmpty() && password.matches(context.getString(R.string.a_z_a_z_d_a_za_z_d_6));
+    }
+    public boolean isValidPhone(String phone)
+    {
+        return phone.length() == 10 && phone.matches(context.getString(R.string._0_9_10));
+    }
+    public boolean isValidConfirmPassword(String confirmPassword, String password)
+    {
+        return password.matches(confirmPassword);
+    }
+
+    public boolean isValidRegistration(String name, String email, String password, String phone, String confirmPassword)
+    {
+        return isValidName(name) && isValidEmail(email) && isValidPassword(password) && isValidPhone(phone) && isValidConfirmPassword(password, confirmPassword);
+    }
+}
+
