aa7c775000e15acd717c26177e745806f9c1dd57
AdrianPortaln01489363 <122579247+AdrianPortal9363@users.noreply.github.com>
2023-12-03 21:33:57 -0500

    Created Login validation class for junit.

---
+++ app/src/main/java/ca/teambot/it/cave/examination/bot/LoginValidation.java
@@ -0,0 +1,26 @@
+package ca.teambot.it.cave.examination.bot;
+
+import android.content.Context;
+
+import ca.teambot.it.cave.examination.bot.R;
+
+public class LoginValidation
+{
+    private final Context context;
+
+    public LoginValidation(Context context)
+    {
+        this.context = context;
+    }
+    public boolean isValidEmail(String email) {
+        return !email.isEmpty() && email.matches(context.getString(R.string.a_za_z0_9_a_za_z0_9_a_za_z_2));
+    }
+
+    public boolean isValidPassword(String password) {
+        return !password.isEmpty() && password.matches(context.getString(R.string.a_z_a_z_d_a_za_z_d_6));
+    }
+
+    public boolean isValidLogin(String email, String password) {
+        return isValidEmail(email) && isValidPassword(password);
+    }
+}
