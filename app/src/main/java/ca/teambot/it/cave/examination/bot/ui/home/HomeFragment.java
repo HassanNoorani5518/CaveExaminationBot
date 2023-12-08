97641f1c8e6cf5dfabd221c2c768b94b4d6947d4
Hassan Noorani <hanoorani1@gmail.com>
2023-12-08 15:56:09 -0500

    Binded floating button to data reading fragement.

--- app/src/main/java/ca/teambot/it/cave/examination/bot/ui/home/HomeFragment.java
+++ app/src/main/java/ca/teambot/it/cave/examination/bot/ui/home/HomeFragment.java
@@ -21,11 +21,15 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import ca.teambot.it.cave.examination.bot.Datareading;
 import ca.teambot.it.cave.examination.bot.FeedbackTimer;
 import ca.teambot.it.cave.examination.bot.LoginActivity;
 import ca.teambot.it.cave.examination.bot.MainActivity;
@@ -42,6 +46,9 @@ public class HomeFragment extends Fragment
     FirebaseAuth auth;
     FirebaseUser user;
     String userId = "";
+    Datareading datareading = new Datareading();
+
+
 
     public HomeFragment()
     {
@@ -51,7 +58,11 @@ public class HomeFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        
+        FloatingActionButton FAB = view.findViewById(R.id.fab);
 
+
+        FAB.setOnClickListener(v -> ButtonNavigateDataReadingFragment());
         textView = view.findViewById(R.id.textView6);
         imageView = view.findViewById(R.id.imageView);
 
@@ -119,4 +130,15 @@ public class HomeFragment extends Fragment
                     .into(imageView);
         }
     }
+
+    private void ButtonNavigateDataReadingFragment()
+    {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.activity_main, datareading);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
 }
